e5d05e76dfb9bdcbfab7ab8a476ea1afd508d92d
mabbiii <rmuneeb85@gmail.com>
2022-04-07 12:25:28 +0500

    enviroment setup - flask/psycopg2

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("IP" , "0.0.0.0")
+os.environ.setdefault("PORT" , "5000")
+os.environ.setdefault("SECRECT_KEY" , "any_secrect_key")
+os.environ.setdefault("DEBUG" , "True")
+os.environ.setdefault("DEVELOPMENT" , "True")
+os.environ.setdefault("DB_URL" , "postgresql://postgres:Arif1234@localhost:5432/taskmanager")
