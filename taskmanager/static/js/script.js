ad2508c529b356e3a1e117139b79f0e6ff18daf4
mabbiii <rmuneeb85@gmail.com>
2022-04-10 15:32:18 +0500

    Edit Task successfuly

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // sidenav initialization
+  var sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  var datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" },
+  });
+
+  // select initialization
+  var selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
+  var collapsibles = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsibles);
+});
